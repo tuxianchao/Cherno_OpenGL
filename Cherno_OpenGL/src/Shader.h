20f0db47e048fa43844e6cfd81a82f2a79a12286
tutu <xianchaot@gmail.com>
2022-08-28 22:04:02 +0800

    fix: type error

--- Cherno_OpenGL/src/Shader.h
+++ Cherno_OpenGL/src/Shader.h
@@ -25,23 +25,23 @@ private:
 	std::string m_filePath;
 	unsigned int m_RenderID;
 	// cacheing for uniforms
-	std::unordered_map<std::string, unsigned int> m_UniformLocaltionCache;
+	std::unordered_map<std::string, int> m_UniformLocaltionCache;
 
 public:
-	Shader(const std::string &filePath);
+	Shader(const std::string& filePath);
 	~Shader();
 
 	void Bind() const;
 	void UnBind() const;
 
 	// set Uniforms
-	void SetUniform4f(const std::string &name, float v0, float v1, float v2, float v3);
-	void SetUniform1i(const std::string &name, unsigned int value);
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniform1i(const std::string& name, unsigned int value);
 
 private:
 	bool CompileShader();
-	int GetUniformLocation(const std::string &name);
-	unsigned int CreateShader(const std::string &vertextShader, const std::string &fragmentShader);
-	unsigned int CompileShader(unsigned int type, const std::string &source);
-	ShaderProgramSource ParseShader(const std::string &filePath);
+	int GetUniformLocation(const std::string& name);
+	unsigned int CreateShader(const std::string& vertextShader, const std::string& fragmentShader);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	ShaderProgramSource ParseShader(const std::string& filePath);
 };
